0766a342064b3c962e757e05a5dc34fdcfefd02b
Jungho Bang <jungho.bang@coinbase.com>
2020-04-15 17:34:56 -0700

    adding files

---
+++ SomeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SomeApp
+//
+//  Created by Jungho Bang on 4/15/20.
+//  Copyright © 2020 Coinbase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
